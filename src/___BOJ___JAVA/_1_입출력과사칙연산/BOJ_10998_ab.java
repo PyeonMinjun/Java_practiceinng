715e0e9d8ddbf10d848b9b47c4a22a47995707ec
PyeonMinjun <alswns9810@gmail.com>
2023-02-15 17:59:27 +0900

    23/02/15

--- src/___BOJ___JAVA/_1_입출력과사칙연산/BOJ_10998_ab.java
+++ src/___BOJ___JAVA/_1_입출력과사칙연산/BOJ_10998_ab.java
@@ -1,21 +1,20 @@
 package ___BOJ___JAVA._1_입출력과사칙연산;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-
-public class BOJ_1000_AB {
+public class BOJ_10998_ab {
     public static void main(String[] args) throws IOException {
-
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); // 고정
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int p[] = new int[2];
 
-        for (int i = 0; i < 2; i++) {
-            p[i] = Integer.parseInt(st.nextToken());
-        }
-        System.out.println(p[0]+ p[1]);
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+
+        System.out.println(a*b);
+
 
 
     }
